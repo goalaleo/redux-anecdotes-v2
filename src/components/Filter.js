fe0d0965e5f8bc6e392673117f9dc376ba93587a
goalaleo <goalaleo@gmail.com>
2018-03-11 20:04:43 +0200

    Improve Filter setFilter function

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -6,10 +6,11 @@ const Filter = (props) => {
   const style = {
     marginBottom: 10
   }
+  const setFilter = (e) => props.setFilter(e.target.value)
 
   return (
     <div style={style}>
-      filter <input onChange={(event) => props.setFilter(event.target.value)}/>
+      filter <input onChange={setFilter}/>
     </div>
   )
 }
